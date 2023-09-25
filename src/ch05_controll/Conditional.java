50e2d7f5e472de333e18b2e3f28d07e640ca7d34
choijangseon <jangseon6@gmail.com>
2023-09-25 09:46:36 +0900

    09.25 추가

---
+++ src/ch05_controll/Conditional.java
@@ -0,0 +1,50 @@
+package ch05_controll;
+
+public class Conditional {
+
+    public static void main(String[] args) {
+
+        // 조건문
+
+        // if문
+        int price = 30000;
+        int myMoney = 10000;
+
+        // myMoney가 price보다 작으면 "돈이 부족합니다" 문구 출력
+        // 조건 안에는 boolean 타입의 결과가 들어간다.
+        // 프로그래밍에서 괄호가 열렸으면 닫히는 부분이 있어야한다
+        if(myMoney < price){
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
